1be0f427efaa1a0ac1132d68885496f7a55fe9ab
tiankay999 <tiankay999@gmail.com>
2025-08-20 16:53:42 +0000

    DOCS Made diferent

---
+++ Script.js
@@ -0,0 +1,6 @@
+
+        document.querySelectorAll('.add-to-cart').forEach(function(btn) {
+            btn.addEventListener('click', function() {
+                alert('Added to cart!');
+            });
+        });
